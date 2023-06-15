2137fa103a60de8df7ce1179bc92c8f0d4881364
Positivemindset <35690220+Positivemindset@users.noreply.github.com>
2023-06-15 09:40:54 +0100

    Update Function.cs

--- Function.cs
+++ Function.cs
@@ -1,45 +1,43 @@
-using Google.Cloud.Functions.Framework;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace dotnetHttpFunction;
-
-public class Function : IHttpFunction
+namespace DotNetCf
 {
-    private readonly ILogger _logger;
-
-    public Function(ILogger<Function> logger) =>
-        _logger = logger;
-
-    public async Task HandleAsync(HttpContext context)
+    public static class Function
     {
-        HttpRequest request = context.Request;
-        // Check URL parameters for "message" field
-        string message = request.Query["message"];
-
-        // If there's a body, parse it as JSON and check for "message" field.
-        using TextReader reader = new StreamReader(request.Body);
-        string text = await reader.ReadToEndAsync();
-        if (text.Length > 0)
+        [FunctionName("Run")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
         {
-            try
+            // Check URL parameters for "message" field
+            string message = req.Query["message"];
+
+            // If there's a body, parse it as JSON and check for "message" field.
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            if (!string.IsNullOrEmpty(requestBody))
             {
-                JsonElement json = JsonSerializer.Deserialize<JsonElement>(text);
-                if (json.TryGetProperty("message", out JsonElement messageElement) &&
-                    messageElement.ValueKind == JsonValueKind.String)
+                try
                 {
-                    message = messageElement.GetString();
+                    JsonElement json = JsonSerializer.Deserialize<JsonElement>(requestBody);
+                    if (json.TryGetProperty("message", out JsonElement messageElement) &&
+                        messageElement.ValueKind == JsonValueKind.String)
+                    {
+                        message = messageElement.GetString();
+                    }
+                }
+                catch (JsonException parseException)
+                {
+                    log.LogError(parseException, "Error parsing JSON request");
                 }
             }
-            catch (JsonException parseException)
-            {
-                _logger.LogError(parseException, "Error parsing JSON request");
-            }
-        }
 
-        await context.Response.WriteAsync(message ?? "Hello World!");
+            return new OkObjectResult(message ?? "Hello World!");
+        }
     }
 }
